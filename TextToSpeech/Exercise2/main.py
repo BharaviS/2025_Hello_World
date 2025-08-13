2f9f27786011c74e58b0efaea1e25db274c12f1c
Bharavi <bharavi0606@gmail.com>
2025-08-13 20:26:51 +0530

    main

--- TextToSpeech/Exercise2/main.py
+++ TextToSpeech/Exercise2/main.py
@@ -1,18 +1,18 @@
 import pyttsx3
 
-engine = pyttsx3.init()
-
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-engine.setProperty('rate', 130)
-engine.setProperty('volume', 1)
-
 class PromptReader:
     def __init__(self, audio) -> None:
         self.__audio = audio
 
     @property
     def speak(self) -> None:
+        engine = pyttsx3.init()
+
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[1].id)
+        engine.setProperty('rate', 130)
+        engine.setProperty('volume', 1)
+
         engine.say(self.__audio)
         engine.runAndWait()
 
--- Tkinter/Exercise1/main.py
+++ Tkinter/Exercise1/main.py
@@ -1,4 +1,26 @@
 import tkinter as tk
+import pyttsx3
+
+class PromptReader:
+    def __init__(self, audio) -> None:
+        self.__audio = audio
+
+    @property
+    def speak(self) -> None:
+        try:
+            engine = pyttsx3.init()
+
+            voices = engine.getProperty('voices')
+            engine.setProperty('voice', voices[1].id)
+            engine.setProperty('rate', 130)
+            engine.setProperty('volume', 1)
+
+            engine.say(self.__audio)
+            engine.runAndWait()
+
+            return self.__audio
+        except Exception as e:
+            print(e)
 
 root = tk.Tk()
 root.geometry('800x520')
@@ -10,12 +32,20 @@ text.pack(expand=True, fill="both", padx=10, pady=(10, 6))
 controls = tk.Frame(root)
 controls.pack(fill="x", padx=10, pady=(0, 10))
 
-play_btn = tk.Button(controls, text="Play ▶", width=10)#, command = on_play)
-pause_btn = tk.Button(controls, text="Pause ⏸", width=10)#, command= on_pause, state="disabled")
-stop_btn = tk.Button(controls, text="Stop ■", width=10)#, command=on_stop, state="disabled")
+def on_play():
+    try:
+        audio = text.get("1.0", tk.END)
+        prompt = PromptReader(audio).speak
+        print(prompt)
+    except Exception as e:
+        print(e)
+
+play_btn = tk.Button(controls, text="Play ▶", width=10, command = on_play)
+pause_btn = tk.Button(controls, text="Pause ⏸", width=10)#, command= on_pause, state="disabled"
+stop_btn = tk.Button(controls, text="Stop ■", width=10)#, command=on_stop, state="disabled"
 back_btn = tk.Button(controls, text="⏪ 5s", width=7)#, command=lambda: seek(-5), state="disabled")
 forward_btn = tk.Button(controls, text="5s ⏩", width=7)#, command=lambda: seek(5), state="disabled")
-load_btn = tk.Button(controls, text="Load .txt", width=10)#, command=load_txt)
+load_btn = tk.Button(controls, text="Load .txt", width=10)#, command=load_txt
 
 play_btn.pack(side="left", padx=6)
 pause_btn.pack(side="left", padx=6)
